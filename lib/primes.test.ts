b0276bbe70875cc3037dfd14cd7cf74ba7b23973
Jacob Biggs <biggs.jacob@gmail.com>
2023-02-20 20:09:14 -0600

    Add function to produce a specific number of primes

--- lib/primes.test.ts
+++ lib/primes.test.ts
@@ -1,12 +1,11 @@
 import {describe, expect, test} from '@jest/globals';
-import {sieveOfEratosthenes} from "./primes";
+import {generatePrimes, sieveOfEratosthenes} from "./primes";
 
 describe('sieveOfEratosthenes', () => {
     test('computes primes < 10', () => {
-        const primes = sieveOfEratosthenes(10);
+        const primes = sieveOfEratosthenes(10).primes;
 
-        expect(primes).toHaveLength(5);
-        expect(primes).toContain(1);
+        expect(primes).toHaveLength(4);
         expect(primes).toContain(2);
         expect(primes).toContain(3);
         expect(primes).toContain(5);
@@ -14,8 +13,19 @@ describe('sieveOfEratosthenes', () => {
     });
 
     test('computes a large prime', () => {
-        const primes = sieveOfEratosthenes(1_000_000);
+        const primes = sieveOfEratosthenes(1_000_000).primes;
 
         expect(primes).toContain(999983);
     })
+});
+
+const PRIME_ONE_MILLION = 15485863;
+describe('generatePrimes', () => {
+   test('can generate one million primes', () => {
+       const primes = generatePrimes(1_000_000);
+
+       console.log(primes[999_999]);
+       expect(primes).toHaveLength(1_000_000);
+       expect(primes).toContain(PRIME_ONE_MILLION);
+   })
 });
--- lib/primes.ts
+++ lib/primes.ts
@@ -5,10 +5,16 @@
  * Wiki: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
  *
  * @param n Upper bound for prime value
+ * @param previousSieve Resulting sieve from previous run, to save some work
  * @returns {*[]}
  */
-export function sieveOfEratosthenes(n: number): number[] {
+export function sieveOfEratosthenes(n: number, previousSieve?: Uint8Array): SieveResult {
     const isPrime = (new Uint8Array(n)).fill(1);
+    if (previousSieve) {
+        previousSieve.forEach((v, i) => {
+            isPrime[i] = v;
+        });
+    }
     const sqrtN = Math.sqrt(n);
 
     for (let i = 2; i < sqrtN; i++) {
@@ -21,11 +27,33 @@ export function sieveOfEratosthenes(n: number): number[] {
         }
     }
 
-    return isPrime.reduce((primes, primeness, number) => {
-        if (primeness === 1) {
-            primes.push(number);
-        }
+    return {
+        sieveArray: isPrime,
+        primes: isPrime.reduce((primes, primeness, number) => {
+            if (primeness === 1) {
+                primes.push(number);
+            }
 
-        return primes;
-    }, [] as number[]);
+            return primes;
+        }, [] as number[]).slice(2) // chop 0 and 1
+    };
+}
+
+export function generatePrimes(n: number): number[] {
+    let bound = n * 14;
+    let sieve = undefined;
+    let primes = [];
+    do {
+        const sieveResult = sieveOfEratosthenes(bound, sieve);
+        sieve = sieveResult.sieveArray;
+        primes = sieveResult.primes;
+        bound *= 2;
+    } while (primes.length < n)
+
+    return primes.slice(0, n);
+}
+
+export type SieveResult = {
+    sieveArray: Uint8Array
+    primes: number[]
 }
